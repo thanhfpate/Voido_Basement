a6dd69af8df3a0a6dd44c7b599750c2aefad7763
thanhfpate <Tangtienthanh208@gmail.com>
2025-05-26 21:21:34 +0700

    init

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -3,10 +3,9 @@ extends Control
 
 
 
-func _on_playbutton_pressed():
+func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scene/map test.tscn")
-
-
+	pass # Replace with function body.
 
 
 func _on_exitbutton_pressed():
